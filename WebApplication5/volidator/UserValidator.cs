0523dad5c6567fc039865803c673fe053d93f9d5
Shahriyor Sattorov <shaha4aa@gmail.com>
2024-08-04 00:00:54 +0300

    commit

---
+++ WebApplication5/volidator/UserValidator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Net.Http.Headers;
+using WebApplication5.Entity;
+using FluentValidation;
+using System.Data;
+using Microsoft.Identity.Client;
+using System.Text.RegularExpressions;
+
+namespace WebApplication5.volidator
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator() 
+        {
+            //string NameValidation = "1234567890!@#$%^&*().,:;";
+
+            //Regex regex = new Regex("^[A-Z А-Я][a-z]");
+            //Match matches = regex.Matches(name);
+            //if (matches.Success)
+            //{
+
+            //}
+
+            RuleFor(user => user.Name).NotEmpty().Matches("^[A-Za-zА-Яа-яЁё\\s'-]+$").WithMessage("Name is required.").Length(2, 50).NotEqual("1234567890");
+            RuleFor(user => user.Surname).NotEmpty().WithMessage("Surname is required.");
+            RuleFor(user => user.Name).NotEmpty().WithMessage("Name is required.");
+            RuleFor(user => user.Email).NotEmpty().WithMessage("Email is required").EmailAddress();
+        }
+    }
+}
